0d3deb35d252e3115c993c66291f103623e36ab5
feli0605 <felixhasal@gmail.com>
2020-10-23 09:51:15 +0200

    lyskryds
    sådan tegner man et lydskryds

---
+++ loop_lol_2020_10_23_06_56_45/sketch.js
@@ -0,0 +1,29 @@
+let x = 0;
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+}
+function draw() {
+  background(19,24,98);
+ let c = color(0, 0, 0);
+fill(c);
+noStroke();
+  rect(119, 200, 20, 265);
+  rect(100, 60, 60, 140, 20);
+  
+   let d = color(100, 0, 0);
+fill(d);
+noStroke();
+  circle(130, 90, 30);
+  
+ let e = color(155, 104, 0);
+fill(e);
+noStroke();
+  circle(130, 130, 30);
+  
+  let f = color(0, 100, 0);
+fill(f);
+noStroke();
+  circle(130, 170, 30);
+}
+
